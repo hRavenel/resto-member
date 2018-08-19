6dff33e859a1c4ccf0f905db4d83aeb58b6ef283
ravenel.h <ravenel.h@sfeir.com>
2018-08-19 20:34:56 +0200

    test travis

--- src/components/postResto.js
+++ src/components/postResto.js
@@ -1,10 +1,10 @@
-postResto () {
-  this.$root.$firebaseRefs.resto.push(
-    {
-      'url': this.restoUrl,
-      'comment': this.title,
-      'info': 'Posted by Charles on Tuesday',
-      'created_at': -1 * new Date().getTime()
-    })
-    .then(this.$router.push('/'))
-}
+// postResto () {
+//   this.$root.$firebaseRefs.resto.push(
+//     {
+//       'url': this.restoUrl,
+//       'comment': this.title,
+//       'info': 'Posted by Charles on Tuesday',
+//       'created_at': -1 * new Date().getTime()
+//     })
+//     .then(this.$router.push('/'))
+// }
